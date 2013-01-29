9426bf0abad552e9a78b4a7dbb161ff06277d2f0
kulikov <kulikov.dm@gmail.com>
2013-01-29 20:19:00 +0400

    eventsourced actor as inner class

--- core/src/main/scala/com/libitec/sce/core/Main.scala
+++ core/src/main/scala/com/libitec/sce/core/Main.scala
@@ -3,95 +3,94 @@ package com.libitec.sce.core
 import java.io.File
 
 import scala.concurrent.duration._
-import scala.language.implicitConversions
 
 import akka.actor._
-import akka.pattern.ask
-import akka.util.Timeout
 
 import org.eligosource.eventsourced.core._
 import org.eligosource.eventsourced.journal._
-import scala.concurrent.ExecutionContext
 
 
 object Main extends App {
 
   println("Start...")
 
-  implicit val system = ActorSystem("example")
-
-  val doerJournal = Journal(JournalioJournalProps(new File("target/journal/doers")))
-  val doerExtension = EventsourcingExtension(system, doerJournal)
-
-  val doerStore = doerExtension.processorOf(ProcessorProps(new DoerStore with Receiver with Eventsourced { val id = 1 } ))
-  val doer = system.actorOf(Props(new Doer(doerStore)), name = "doer-1")
-
-  doerExtension.recover()
-
-
-
-
-  transaction { sync ⇒
-    doer ! sync(LoadSku("5 books"))
-    node ! sync(UnloadSku("5 books"))
-  }
-
-
-
-
-
+  new Production().init()
 }
 
 
-//
-//
-//case class LoadSku(sku: String)
-//
-//
-//class Doer(doerStore: ActorRef) extends Actor {
-//
-//  lazy val payload = Await.ready(doerStore ? GetPaylod, 10 seconds).asInstanceOf[Payload]
-//
-//  doerStore.underlaingActor.payload
-//
-//  def receive = {
-//    case sync @ LoadSku(sku) ⇒
-//      implicit val timeout = Timeout(1 second)
-//
-//      if (!payload.hasSku(sku)) {
-//
-//        store(LoadSku(sku), sync) { p =>
-//          payload = p
-//        }
-//      } else
-//        sync.fail("Sku already has!")
-//  }
-//
-//  private def store[T](sync: SympleSync, msg: Any)(callback: ⇒ T): T = {
-//    if (sync.ok()) {
-//      Await.ready(doerStore ? Message(msg)) onComplete {
-//        case Right(Success(msg)) ⇒
-//          try callback()
-//          catch { case e: Exception ⇒
-//            doerStore ! Revert(msg)
-//          }
-//        case error ⇒
-//          throw new Exception
-//      }
-//    } else {
-//      log.error("Transaction failed!")
-//    }
-//  }
-//}
-//
-//
-//class DoerStore extends Actor {
-//  var payload = new Payload
-//
-//  def receive = {
-//    case LoadSku(sku) ⇒
-//      payload.addSku(sku)
-//      println("[Doer] event = %s (%s)" format (msg, payload))
-//      sender ! "ok"
-//  }
-//}
+class Production
+  extends ActorSystemComponent
+  with DoerComponent {
+
+  def init() {
+
+    val doer1 = createDoer(1)
+    val doer2 = createDoer(2)
+
+    transaction { sync ⇒
+      doer1 ! sync(LoadSku("Sku for doer1"))
+      doer2 ! sync(LoadSku("Sku for doer2"))
+    }
+  }
+}
+
+
+trait ActorSystemComponent {
+  implicit val actorSystem = ActorSystem("eventsourced-example")
+}
+
+
+case class LoadSku(sku: String)
+
+
+
+
+
+trait DoerComponent {
+  this: ActorSystemComponent ⇒
+
+
+  def createDoer(id: Long) =
+    actorSystem.actorOf(Props(new Doer(id)), name = s"doer-$id")
+
+  val doerJournal = Journal(JournalioJournalProps(new File("target/journal/doers")))
+  val doerExtension = EventsourcingExtension(actorSystem, doerJournal)
+
+
+  /**
+   * Actor
+   */
+  class Doer(doerId: Long) extends Actor {
+
+    var payload = Set.empty[String]
+    var cnt = 0
+
+    val doerStore = doerExtension.processorOf(Props(new DoerStore with Receiver with Eventsourced { val id = doerId.toInt } ))(context)
+    doerExtension.recover(id ⇒ if (id == doerId) Some(0) else None, 1 minute)
+
+
+    class DoerStore extends Actor {
+      def receive = {
+        case LoadSku(sku) ⇒
+          cnt += 1
+          payload += s"$sku; - $cnt"
+          println(s"[Doer] event = $sku ($payload)")
+
+        case Snapshot(bites: Array[Byte]) {
+          val p = serializer.fromBinary()
+          cnt = p.getCnt
+        }
+      }
+    }
+
+
+    def receive =  {
+      case LoadSku(sku) ⇒
+        doerStore ? Message(LoadSku(sku + ";") onSuccess {
+          case e ⇒
+            payload.
+        }
+    }
+
+  }
+}
